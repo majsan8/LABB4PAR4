f991f692e113530f009a2f489043a90d55ac98cf
majsan8 <maja.lifh01@gmail.com>
2024-12-16 10:29:12 +0100

    enum

---
+++ Gender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABB4PAR4
+{
+    public enum Gender
+    {
+        Woman,
+        Man,
+        None
+    }
+}
