4f66b8fcb36648f25ae6ece28d35e860d0bb0988
emilgrupp16 <emil.johansson62@hotmail.com>
2024-12-16 10:29:20 +0100

    struct

---
+++ Hair.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABB4PAR4
+{
+    public struct Hair
+    {
+        public int HairLength;
+        public string HairColor;
+        public string HairStyle;
+    }
+}
