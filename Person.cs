aa56b89df2dbb114ddd4b4c06a9f95d1ec0ea2ac
majsan8 <maja.lifh01@gmail.com>
2024-12-16 12:17:11 +0100

    startsprinttwomenu

---
+++ Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABB4PAR4
+{
+    public class Person
+    {
+        public int Birthday { get; set; }
+        public string EyeColor { get; set; }
+
+        public Gender gender {  get; set; }
+        public Hair hair {  get; set; }
+
+        //Konstruktor
+        public Person(int Birthday, string Eyecolor, Gender gender, Hair hair)
+        {
+           
+
+
+        }
+
+        public override string ToString()
+        {
+            return $"Birthday: {Birthday}\n," +
+                $"Eye color: {EyeColor}\n," +
+                $"Gender: {gender}\n," +
+                $"Hair length: {hair}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
+            /*Person person1 = new(
+                2001-10-30,
+                "Green",
+                Gender.Woman,
+                new Hair { HairLength = 20, HairColor = "Blonde", HairStyle = "Long" });
+
+            Person person2 = new(
+                1997-11-03,
+                "Brown",
+                Gender.Man,
+                new Hair { HairLength = 5, HairColor = "Brown", HairStyle = "Short" });
+                
+              
+                Console.WriteLine(person1.ToString());
+                Console.WriteLine(person2.ToString());^*/
             
+            Console.WriteLine("Välkommen!");
+            int userChoice;
+
+            while (true)
+            {
+                Console.WriteLine("\n Du kommer nu att få tre val. Skriv valet med siffra");
+                Console.WriteLine("\n 1. Lägg till person" +
+                                "\n 2. Skriv ut lista med personlig information" +
+                                "\n 3. Avsluta programmet");
+                
+                userChoice = int.Parse(Console.ReadLine());
+
+                switch (userChoice)
+                {
+                    case 1:
+                        {
+                            break;
+                        }
+                    case 2:
+                        {
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Nu avslutas programmet");
+                            Environment.ExitCode = 0;
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+            }
         }
     }
 }
